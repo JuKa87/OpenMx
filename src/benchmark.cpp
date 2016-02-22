f055df183ca433abd194e494a433142825666128
Julian Karch <karch@mpib-berlin.mpg.de>
2016-02-22 22:13:32 +0100

    changes by michael krause

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -11,11 +11,18 @@
 std::stack<clock_t> tictoc_stack;
 
 void tic() {
+    #pragma omp critical
+    {
     tictoc_stack.push(clock());
+    }
 }
 
 double toc() {
-    clock_t tmp=tictoc_stack.top();
+    clock_t tmp;
+    #pragma omp critical
+    {
+    tmp = tictoc_stack.top();
     tictoc_stack.pop();
+    }
     return(((double)(clock() - tmp)) / CLOCKS_PER_SEC);
 }
